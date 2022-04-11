fd5c131d1f8b0a8a5042b1776adccee3a0615bdd
kayblessgit <positivemindsoar@gmail.com>
2022-04-11 14:25:43 -0700

    My commit message

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,25 +7,16 @@
 int main(void)
 
 {
-int n;
+char alphabet;
 
-srand(time(0));
-n = rand () - RAND_MAX / 2;
-
-if (n % 10 > 5)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
+putchar(aphabet);
 }
 
-else if (n % 10 == 0)
-{
-printf("Last digit of %i is %i and is 0\n", n, n % 10);
+putchar('\n');
+
+reture (0);
+
 }
 
-else
-{printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
-}
-
-
-return(0);
-}
