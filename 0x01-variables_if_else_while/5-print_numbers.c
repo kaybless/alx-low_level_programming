f38c83cacc491a70fbd658f7756e7d6330940196
kayblessgit <positivemindsoar@gmail.com>
2022-04-11 17:16:50 -0700

    My commit message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{int i;
+
+for (i = 0 ; i < 10; i++)
+{
+printf("%i", i);
+}
+
+printf("\n")
+
+return (0);
+
+}
