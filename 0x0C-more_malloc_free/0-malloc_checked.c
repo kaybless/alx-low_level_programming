b6879bf3ba09b392f5ad0de017ead4edc24114c4
kayblessgit <positivemindsoar@gmail.com>
2022-05-09 13:00:44 -0700

    task 0 commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * File: 0-malloc_checked.c
+ * Auth: Brennan D Baraban
+ */
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+
+	return (mem);
+}
+
