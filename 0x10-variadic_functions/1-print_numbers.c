369d2ec883a185308a6ac462ac8da3a1ffa73207
kayode Adeniji <positivemindsoar@gmail.com>
2022-05-17 22:27:15 +0100

    Update 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - fills memory with a constant byte
+ * @separator: is string to be printed
+ * @n: the amount of args
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    va_list valist;
+    unsigned int i;
+
+    va_start(valist, n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", va_arg(valist, int));
+        if (separator != NULL && i != n - 1)
+            printf("%s", separator);
+    }
+    va_end(valist);
+    printf("\n");
+}
