f1cb1448dd9c4076bd0a4957da998774e075e1cf
kayode Adeniji <positivemindsoar@gmail.com>
2022-05-17 22:38:58 +0100

    Update 2-prints_strings.c

--- 0x10-variadic_functions/2-prints_strings.c
+++ 0x10-variadic_functions/2-prints_strings.c
@@ -24,7 +24,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
         else
             printf("(nil)");
 
-        if (separator != NULL && i != n - 1)
+            if (i < n - 1)
+        if (separator)
             printf("%s", separator);
     }
     va_end(valist);
