dc422471771427fa47c4d45948478e49a3919576
kayode Adeniji <positivemindsoar@gmail.com>
2022-06-01 19:45:09 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,24 @@
 #include "main.h"
-
-**
- * factorial - gets factorial of n
- * @n: integer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+/**
+ * factorial - factorial of int
+ * @n:int
+ * Return:int
  */
 int factorial(int n)
 {
-	if (n < 0)
-		return (-1);
+	int x;
+
 	if (n == 0)
+	{
 		return (1);
-	return (n * factorial(n - 1));
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		x = n * factorial(n - 1);
+	}
+	return (x);
 }
