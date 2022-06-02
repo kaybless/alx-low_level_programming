a11353d7119d8ffffe94ad8bead34c444e6c57ea
kayode Adeniji <positivemindsoar@gmail.com>
2022-06-02 21:30:10 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,23 +18,16 @@ char *text;
 text = malloc(letters);
 if (text == NULL)
 return (0);
-
 if (filename == NULL)
 return (0);
-
 file = open(filename, O_RDONLY);
-
 if (file == -1)
 {
 free(text);
 return (0);
 }
-
 let = read(file, text, letters);
-
 w = write(STDOUT_FILENO, text, let);
-
 close(file);
-
 return (w);
 }
