32f163602d5641353bf59d64ec34ce6bfbeea84c
kayode Adeniji <positivemindsoar@gmail.com>
2022-06-02 21:33:30 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,25 +12,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int i = 0, file;
+int i = 0, file;
 
-    if (filename == NULL)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
-    if (text_content == NULL)
-        text_content = "";
+if (text_content == NULL)
+text_content = "";
 
-    while (text_content[i] != '\0')
-    {
-        i++;
-    }
+while (text_content[i] != '\0')
+{
+i++;
+}
 
-    file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (file == -1)
-        return (-1);
+if (file == -1)
+return (-1);
 
-    write(file, text_content, i);
+write(file, text_content, i);
 
-    return (1);
+return (1);
 }
