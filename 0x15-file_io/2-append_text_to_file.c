cb4bf339ffc1821c7b13dccc8a9e82c7300dace1
kayode Adeniji <positivemindsoar@gmail.com>
2022-06-02 21:35:47 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,25 +11,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int i = 0, file;
+int i = 0, file;
 
-    if (filename == NULL)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
-    if (text_content == NULL)
-        text_content = "";
+if (text_content == NULL)
+text_content = "";
 
-    while (text_content[i] != '\0')
-    {
-        i++;
-    }
+while (text_content[i] != '\0')
+{
+i++;
+}
 
-    file = open(filename, O_WRONLY | O_APPEND);
+file = open(filename, O_WRONLY | O_APPEND);
 
-    if (file == -1)
-        return (-1);
+if (file == -1)
+return (-1);
 
-    write(file, text_content, i);
+write(file, text_content, i);
 
-    return (1);
+return (1);
 }
