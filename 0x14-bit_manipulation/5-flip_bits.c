f8b30c3773d65b78072e30cd658fce2ef5f4b6a8
kayode Adeniji <positivemindsoar@gmail.com>
2022-06-02 22:05:44 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,15 +7,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int t;
-    int i = 0;
+unsigned long int t;
+int i = 0;
 
-    t = n ^ m;
-    while (t >= 1)
-    {
-        if ((t & 1) == 1)
-            i++;
-        t >>= 1;
-    }
-    return (i);
+t = n ^ m;
+while (t >= 1)
+{
+if ((t & 1) == 1)
+i++;
+t >>= 1;
+}
+return (i);
 }
