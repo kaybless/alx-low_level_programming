72f877890dfb713fae5c6c9eeea875815addda9e
kayode Adeniji <positivemindsoar@gmail.com>
2022-06-02 22:00:42 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,11 +7,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (sizeof(n) * 8 < index)
-        return (-1);
-    n >>= index;
-    if ((n & 1) == 1)
-        return (1);
-    else
-        return (0);
+if (sizeof(n) * 8 < index)
+return (-1);
+n >>= index;
+if ((n & 1) == 1)
+return (1);
+else
+return (0);
 }
