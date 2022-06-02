5acfc61bbc6d6871a7619489818b0d586354db30
kayode Adeniji <positivemindsoar@gmail.com>
2022-06-02 22:02:13 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,12 +8,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned int x = 1;
+unsigned int x = 1;
 
-    if (sizeof(n) * 8 < index)
-        return (-1);
+if (sizeof(n) * 8 < index)
+return (-1);
 
-    x <<= index;
-    *n |= x;
-    return (1);
+x <<= index;
+*n |= x;
+return (1);
 }
