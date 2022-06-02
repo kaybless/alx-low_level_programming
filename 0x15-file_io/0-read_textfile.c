78371287fa7b598dbc4b45d52e9f9360ba3ef3b0
kayode Adeniji <positivemindsoar@gmail.com>
2022-06-02 21:24:05 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,29 +13,29 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, let, w;
-    char *text;
+ssize_t file, let, w;
+char *text;
 
-    text = malloc(letters);
-    if (text == NULL)
-        return (0);
+text = malloc(letters);
+if (text == NULL)
+return (0);
 
-    if (filename == NULL)
-        return (0);
+if (filename == NULL)
+return (0);
 
-    file = open(filename, O_RDONLY);
+file = open(filename, O_RDONLY);
 
-    if (file == -1)
-    {
-        free(text);
-        return (0);
-    }
+if (file == -1)
+{
+free(text);
+return (0);
+}
 
-    let = read(file, text, letters);
+let = read(file, text, letters);
 
-    w = write(STDOUT_FILENO, text, let);
+w = write(STDOUT_FILENO, text, let);
 
-    close(file);
+close(file);
 
-    return (w);
+return (w);
 }
